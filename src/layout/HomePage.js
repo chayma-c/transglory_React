5d8a05f14405d4ef9d9efb117cbf5de53a413fd0
aymenjn <aymenjani20@gmail.com>
2024-02-13 19:08:10 +0100

    animation

--- src/layout/HomePage.js
+++ src/layout/HomePage.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React , {useEffect , useState} from 'react'
 import * as s from './home.style.js'
 import Header from './navBar/navBar.js'
-import mainData from './home.data.js'
+import ClipLoader from "react-spinners/ClipLoader";
+import { css } from "@emotion/react";
+
 const HomePage=()=>{
-    const localData = mainData();
+    const [isLoading , setisLoading] = useState(false)
+
+    useEffect(()=>{
+
+
+        setisLoading(true) ; 
+        setTimeout(() => {
+          setisLoading(false) ;
+        
+        
+        }, 4000);
+        },[])
+        const override = css`
+
+  margin-top: 30vh;
+  border-color: white;
+`;
     return (
+        isLoading 
+        ? 
+    <s.Loader >
+    
+    <ClipLoader  color="#ffffff" loading={isLoading}  css={override}  size={350} />
+    </s.Loader>
+     
+    
+    
+        : 
     <s.main_container color="#f5f5f5">
         <Header/>
     </s.main_container>
--- src/layout/home.style.js
+++ src/layout/home.style.js
@@ -4,4 +4,14 @@ text-align:center;
 color:${p=>p.color};
 min-height : 100vh ; 
 background-image: linear-gradient(315deg, #be1622 0%, #2234 100%);
+
+`
+export const Loader = styled.div`
+height  : 100vh ; 
+background-color: #7f5a83;
+background-image: linear-gradient(315deg, #7f5a83 0%, #0d324d 74%);
+
+display  : flex ; 
+justify-content : center ; 
+align-items : center ; 
 `
